5f934be323ed650252ce1c0783ea47dcfed2b7e7
Luís Felipe de Andrade <luis.felipe.de.andrade@gmail.com>
2016-06-11 11:33:08 -0300

    Exercício 2 - Página 18 - Pontos

---
+++ modulo1/src/edu/softmap/Ponto.java
@@ -0,0 +1,10 @@
+package edu.softmap;
+
+/*
+ * Author @softmap
+ */
+public class Ponto {
+   public int coordenateX, coordenateY;
+  
+   
+}
---
+++ modulo1/src/modulo1/Exercicio2Ponto.java
@@ -0,0 +1,22 @@
+package modulo1;
+
+import edu.softmap.Ponto;
+
+public class Exercicio2Ponto {
+	public static void main(String[] args) {
+		   
+		  Ponto inicio,fim;
+		  
+		  inicio = new Ponto();
+		  inicio.coordenateX = 10;
+		  inicio.coordenateY = 10;
+		  
+		  fim = new Ponto();
+		  fim.coordenateX  = 20;
+		  fim.coordenateY  = 30;
+		  
+		  System.out.println("Localizacao inicio ("+ inicio.coordenateX+ ","+inicio.coordenateY + ")");
+		  System.out.println("Localizao fim (" + fim.coordenateX + ","+ fim.coordenateY + ")");
+		  
+	   }
+}
