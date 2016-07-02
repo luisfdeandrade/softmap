837dc2e457a21511affa0a492f74f056d1f2798d
Luís Felipe de Andrade <luis.felipe.de.andrade@gmail.com>
2016-07-02 11:56:54 -0300

    Exercício Java15 - Farol

---
+++ modulo1/src/auto/eletrico/Carro.java
@@ -0,0 +1,22 @@
+package auto.eletrico;
+
+public class Carro {
+	
+	private Lampada farolDireito;
+	private Lampada farolEsqurdo;
+	
+	public Lampada getFarolDireito() {
+		return farolDireito;
+	}
+	public void setFarolDireito(Lampada farolDireito) {
+		this.farolDireito = farolDireito;
+	}
+	public Lampada getFarolEsqurdo() {
+		return farolEsqurdo;
+	}
+	public void setFarolEsqurdo(Lampada farolEsqurdo) {
+		this.farolEsqurdo = farolEsqurdo;
+	}
+
+
+}
